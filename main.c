ce52f6e6d4e37a2b62c1706c6a967f04981c27ca
MatiasPericolo <matiaspericolo@gmail.com>
2020-10-24 12:40:36 -0300

    main()

--- main.c
+++ main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void fEliminar (tElemento elemento){
+
+    free(elemento);
+
+}
+
+void fEliminarC (tClave clave){
+
+    free(clave);
+
+}
+
+void fEliminarV (tValor valor){
+
+    free(valor);
+
+}
+
 int main(int argc,char ** args)
 {
     printf("Hello world!\n");
     printf("Cantidad de parametros: %d\n",argc);
+
+
+
     return 0;
+}
+
+void cantidad_apariciones(){
+
+}
+
+void salir(tMapeo * m,tLista * l){
+
+    l_destruir(l,fEliminar);
+    m_destruir(m,fEliminarC,fEliminarV);
+
 }
